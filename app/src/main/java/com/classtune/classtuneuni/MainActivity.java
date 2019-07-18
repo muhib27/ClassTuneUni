860a611f985a722e71216d7d974f69d098d43684
muhib27 <muhib.raven@gmail.com>
2019-07-18 20:07:08 +0600

    srwer

--- app/src/main/java/com/classtune/classtuneuni/MainActivity.java
+++ app/src/main/java/com/classtune/classtuneuni/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pre_login_layout);
+        setContentView(R.layout.registration_layout);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
