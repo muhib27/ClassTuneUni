4bcd93b7b6fe2af2219eed10b127e8052444360e
muhib27 <muhib.raven@gmail.com>
2019-07-21 20:08:23 +0600

    srwer

--- app/src/main/java/com/classtune/classtuneuni/fragment/AssignmentFragment.java
+++ app/src/main/java/com/classtune/classtuneuni/fragment/AssignmentFragment.java
@@ -1,9 +1,11 @@
 package com.classtune.classtuneuni.fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,7 @@ import com.classtune.classtuneuni.R;
  * A simple {@link Fragment} subclass.
  */
 public class AssignmentFragment extends Fragment {
+    TabLayout tabLayout;
 
 
     public AssignmentFragment() {
@@ -32,5 +35,28 @@ public class AssignmentFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+                tabLayout = (TabLayout) view.findViewById(R.id.tab_layout);
+
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.pigeon_hole_tab_).setTag("pigeonhole"));
+//        if (userPermission.isUserTasksSubmitTask())
+//            tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.submission_tray_tab).setTag("submission_tray"));
+//        else
+//            hideItem();
+//        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.cm_box).setTag("cm_box"));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.pigeon_hole_tab_).setTag("reading_package"));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.pigeon_hole_tab_).setTag("notice"));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.pigeon_hole_tab_).setTag("course_calendar"));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.pigeon_hole_tab_).setTag("events"));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.pigeon_hole_tab_).setTag("research_icon"));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.pigeon_hole_tab_).setTag("profile"));
+//        tabLayout.addTab(tabLayout.newTab().setTag("tab"));
+
+
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            if (tab != null) tab.setCustomView(R.layout.view_home_tab);
+        }
+        tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#74af27"));
     }
 }
