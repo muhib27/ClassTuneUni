7ebca887844a71e02cc3b43ca27743c9db357fa7
riazurcse <riazur90bd@gmail.com>
2019-08-29 06:00:22 +0600

    mac

---
+++ app/src/main/java/com/classtune/classtuneuni/adapter/StHomeAdapter.java
@@ -0,0 +1,288 @@
+package com.classtune.classtuneuni.adapter;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.classtune.classtuneuni.R;
+import com.classtune.classtuneuni.activity.MainActivity;
+import com.classtune.classtuneuni.message.StCourseDiscussion;
+import com.classtune.classtuneuni.model.AssignmentModel;
+import com.classtune.classtuneuni.utils.AppSharedPreference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class StHomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<StCourseDiscussion> mValues;
+    private Context mContext;
+    protected ItemListener mListener;
+    private static final int NOTICE = 1;
+    private static final int ASSIGNMENT = 0;
+    private static final int EXAM = 2;
+
+    private static String PREVIOUS_ID = "";
+    private static  String CURRENT_ID = "";
+
+    public StHomeAdapter(Context context) {
+        mValues = new ArrayList<>();
+        mContext = context;
+
+    }
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(mContext).inflate(R.layout.row_item, parent, false);
+//        return new ViewHolder(view);
+        RecyclerView.ViewHolder viewHolder = null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
+        switch (viewType) {
+            case ASSIGNMENT:
+                View viewAssignment = inflater.inflate(R.layout.home_assignment_item_row, parent, false);
+                viewHolder = new AssignmentView(viewAssignment);
+                break;
+
+            case NOTICE:
+                View viewHero = inflater.inflate(R.layout.home_notice_item_row, parent, false);
+                viewHolder = new HeroVH(viewHero);
+                break;
+            case EXAM:
+                View viewExam = inflater.inflate(R.layout.home_exam_item_row, parent, false);
+                viewHolder = new ExamView(viewExam);
+                break;
+        }
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, final int position) {
+        final StCourseDiscussion result = mValues.get(position);
+        switch (getItemViewType(position)) {
+
+            case ASSIGNMENT:
+                final AssignmentView assignmentHolder = (AssignmentView) viewHolder;
+                if(position<=0)
+                    PREVIOUS_ID = result.getSenderId();
+                else
+                    PREVIOUS_ID = mValues.get(position-1).getSenderId();
+                CURRENT_ID = result.getSenderId();
+                assignmentHolder.title.setText(result.getContent());
+                assignmentHolder.name.setText(result.getSenderName());
+
+                if(position>0 && PREVIOUS_ID.equals(CURRENT_ID)) {
+                    assignmentHolder.pic.setVisibility(View.INVISIBLE);
+                    assignmentHolder.name.setVisibility(View.GONE);
+                }
+
+
+//                String str = "";
+//                if (AppSharedPreference.getUserType().equals("3")) {
+//                    str = result.getAssignment().getCreatedAt();
+//
+//                    String parts[] = str.split(" ");
+//
+//                    itemHolder.assignedDate.setText(AppUtility.getDateString(parts[0], AppUtility.DATE_FORMAT_APP, AppUtility.DATE_FORMAT_SERVER));
+//                    if (result.getAssignment().getDueDate() != null)
+//                        itemHolder.dueDate.setText(AppUtility.getDateString(result.getAssignment().getDueDate(), AppUtility.DATE_FORMAT_APP, AppUtility.DATE_FORMAT_SERVER));
+//                    if (result.getAssignment().getCourseCode() != null)
+//                        itemHolder.subject.setText(result.getAssignment().getCourseCode());
+//                    if (result.getAssignment().getSectionName() != null)
+//                        itemHolder.section.setText(result.getAssignment().getSectionName());
+//                    if (result.getSubmission() != null && result.getSubmission()== 0) {
+//                        itemHolder.imgLl.setVisibility(View.VISIBLE);
+//                        itemHolder.numLl.setVisibility(View.GONE);
+//                        if (result.getMark() != null)
+//                            itemHolder.img.setImageDrawable(mContext.getResources().getDrawable(R.drawable.not_examined));
+//                        else
+//                            itemHolder.img.setImageDrawable(mContext.getResources().getDrawable(R.drawable.not_submited));
+//                    } else {
+//                        itemHolder.present.setText("" + result.getSubmission());
+//                        if (result.getMark() != null)
+//                            itemHolder.total.setText("" + result.getMark());
+//                    }
+//
+//                    itemHolder.cardView.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View view) {
+//                            Fragment fragment = new AssignmentDetailsFragment();
+//                            gotoFragment(fragment, "assignmentDetailsFragment", result.getAssignment().getId());
+//
+////                        if (mListener != null) {
+////                            mListener.onItemClick(result, position);
+////                        }
+//                        }
+//                    });
+//                } else {
+//                    str = result.getAssignment().getAssignDate();
+//
+//                    String parts[] = str.split(" ");
+//
+//                    itemHolder.assignedDate.setText(AppUtility.getDateString(parts[0], AppUtility.DATE_FORMAT_APP, AppUtility.DATE_FORMAT_SERVER));
+//                    if (result.getAssignment().getDueDate() != null)
+//                        itemHolder.dueDate.setText(AppUtility.getDateString(result.getAssignment().getDueDate(), AppUtility.DATE_FORMAT_APP, AppUtility.DATE_FORMAT_SERVER));
+//                    if (result.getAssignment().getCourseCode() != null)
+//                        itemHolder.subject.setText(result.getAssignment().getCourseCode());
+//                    if (result.getAssignment().getSectionName() != null)
+//                        itemHolder.section.setText(result.getAssignment().getSectionName());
+//                    if (result.getSubmissions() != null)
+//                        itemHolder.present.setText("" + result.getSubmissions());
+//                    if (result.getStudents() != null)
+//                        itemHolder.total.setText("" + result.getStudents());
+//                    itemHolder.cardView.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View view) {
+//                            Fragment fragment = new TeacherStudentListFragment();
+//                            gotoFragment(fragment, "teacherStudentListFragment", result.getAssignment().getId());
+//
+////                        if (mListener != null) {
+////                            mListener.onItemClick(result, position);
+////                        }
+//                        }
+//                    });
+//                }
+
+
+                break;
+            case NOTICE:
+                final HeroVH heroHolder = (HeroVH) viewHolder;
+                if(position<=0)
+                    PREVIOUS_ID = result.getSenderId();
+                else
+                    PREVIOUS_ID = mValues.get(position-1).getSenderId();
+                CURRENT_ID = result.getSenderId();
+                heroHolder.title.setText(result.getContent());
+                if(position>0 && PREVIOUS_ID.equals(CURRENT_ID))
+                    heroHolder.pic.setVisibility(View.INVISIBLE);
+                break;
+        }
+    }
+
+//    @Override
+//    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+//        viewHolder.setData(mValues.get(position));
+//    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+//        if (position == 0) {
+//            return HERO;
+//        } else
+        if (mValues.get(position).getSenderId().equals(AppSharedPreference.getUserId()))
+            return NOTICE;
+        else
+            return ASSIGNMENT;
+    }
+
+    public interface ItemListener {
+        void onItemClick(AssignmentModel item, int pos);
+    }
+
+
+    protected class AssignmentView extends RecyclerView.ViewHolder {
+        private TextView title;
+        private TextView name;
+        private TextView dueDate; // displays "year | language"
+        private ImageView img;
+        private ProgressBar mProgress;
+        private TextView subject, section, present, total;
+        private FrameLayout itemLayout;
+        CardView cardView;
+        CircleImageView pic;
+        LinearLayout numLl, imgLl;
+
+        public AssignmentView(View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.text);
+            pic = itemView.findViewById(R.id.pic);
+            name = itemView.findViewById(R.id.name);
+//            assignedDate = itemView.findViewById(R.id.assignedDate);
+//            dueDate = itemView.findViewById(R.id.dueDate);
+//            subject = itemView.findViewById(R.id.subject);
+//            section = itemView.findViewById(R.id.section);
+//            present = itemView.findViewById(R.id.present);
+//            total = itemView.findViewById(R.id.total);
+//            img = itemView.findViewById(R.id.img);
+////            assignedDate = itemView.findViewById(R.id.assignedDate);
+//            cardView = itemView.findViewById(R.id.cardView);
+//            imgLl = itemView.findViewById(R.id.imgLl);
+//            numLl = itemView.findViewById(R.id.numLl);
+
+        }
+    }
+
+    protected class HeroVH extends RecyclerView.ViewHolder {
+        private TextView title;
+        private TextView mMovieDesc;
+        private TextView mYear; // displays "year | language"
+        private ImageView mPosterImg;
+        private CardView cardView;
+        CircleImageView pic;
+
+        public HeroVH(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.text);
+            pic = itemView.findViewById(R.id.pic);
+        }
+    }
+
+    protected class ExamView extends RecyclerView.ViewHolder {
+        private TextView title;
+        private TextView mMovieDesc;
+        private TextView mYear; // displays "year | language"
+        private ImageView mPosterImg;
+        private CardView cardView;
+        CircleImageView pic;
+
+        public ExamView(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.text);
+            pic = itemView.findViewById(R.id.pic);
+        }
+    }
+
+    public void add(StCourseDiscussion r) {
+        mValues.add(r);
+        notifyItemInserted(mValues.size() - 1);
+    }
+
+    public void addAllData(List<StCourseDiscussion> moveResults) {
+        for (StCourseDiscussion result : moveResults) {
+            add(result);
+        }
+    }
+
+    private void gotoFragment(Fragment fragment, String tag, String assignmentId) {
+        // load fragment
+        Bundle bundle = new Bundle();
+        bundle.putString("assignmentId", assignmentId);
+        fragment.setArguments(bundle);
+        FragmentTransaction transaction = ((MainActivity) mContext).getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.mainContainer, fragment, tag);
+        //transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
--- app/src/main/java/com/classtune/classtuneuni/fragment/HomeFragment.java
+++ app/src/main/java/com/classtune/classtuneuni/fragment/HomeFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -14,12 +17,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.classtune.classtuneuni.R;
+import com.classtune.classtuneuni.adapter.ExamListAdapter;
+import com.classtune.classtuneuni.adapter.StHomeAdapter;
+import com.classtune.classtuneuni.home.StHomeFeed;
 import com.classtune.classtuneuni.home.StHomeRespons;
+import com.classtune.classtuneuni.model.ExamInfoModel;
 import com.classtune.classtuneuni.response.StSectionListResponse;
 import com.classtune.classtuneuni.retrofit.RetrofitApiClient;
 import com.classtune.classtuneuni.utils.AppSharedPreference;
 import com.classtune.classtuneuni.utils.NetworkConnection;
 import com.classtune.classtuneuni.utils.UIHelper;
+import com.classtune.classtuneuni.utils.VerticalSpaceItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -27,9 +38,15 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Response;
 
+import static com.classtune.classtuneuni.activity.MainActivity.GlobalOfferedCourseSectionId;
+
 
 public class HomeFragment extends Fragment {
+    RecyclerView recyclerView;
+    private List<StHomeFeed> stHomeFeedList;
+    LinearLayoutManager linearLayoutManager;
 
+    StHomeAdapter stHomeAdapter;
 
     private TextView nextClassTime;
     UIHelper uiHelper;
@@ -51,11 +68,30 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         uiHelper = new UIHelper(getActivity());
 
+        stHomeFeedList = new ArrayList<>();
+
         nextClassTime = view.findViewById(R.id.nextClassTime);
         //nextClassTime.setText("10:30" + Html.fromHtml("<sub>am</sub>"));
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+
+
+
+        stHomeAdapter = new StHomeAdapter(getActivity());
+        linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.addItemDecoration(new VerticalSpaceItemDecoration(getResources()));
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(stHomeAdapter);
+
+        if(AppSharedPreference.getUserType().equals("3")) {
+            callStudentHomeApi();        }
+        else {
+
+        }
     }
 
-    private void callStudentSectionListApi() {
+    private void callStudentHomeApi() {
 
 
         if (!NetworkConnection.getInstance().isNetworkAvailable()) {
